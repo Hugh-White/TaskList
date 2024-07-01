f6cc423ce4cabce9de9077917248afc800823f57
Hugh White <30012235@student.toiohomai.ac.nz>
2024-07-02 10:02:20 +1200

    Installed fody for auto property changes & built the section where tasks are located. Currently all tasks are shown. When styling begins, I'll bind the task sections collection view to a new observable collection of tasks related to a selected category, and implement a show all tasks button too which displays the entire task list like now.

--- TaskList/MVVM/ViewModels/MainViewModel.cs
+++ TaskList/MVVM/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PropertyChanged;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -8,6 +9,7 @@ using TaskList.MVVM.Models;
 
 namespace TaskList.MVVM.ViewModels
 {
+    [AddINotifyPropertyChangedInterface]
     public class MainViewModel
     {
         #region Properties
